c0935a946deb78a7f716f807d8d44913adc9224a
vaffonso <vaffonso@br.ibm.com>
2019-08-30 15:46:04 -0300

    alternative solution for chessboard

--- exercises/chapter2/Chessboard/index.js
+++ exercises/chapter2/Chessboard/index.js
@@ -7,29 +7,60 @@
 
 const chessboard = (binding = 8) => {
     const lineBreaker = '\n';
-    const gridTotal = binding * binding;
 
     let whiteCell = ' ';
     let blackCell = '#';
     let outputString = '';
 
-    for (let i = 1; i <= gridTotal; i++) {
-        
-        if (i%2 === 0) {
-            outputString = outputString + blackCell;
-        } else {
-            outputString = outputString + whiteCell;
+    for (let i = 0; i < binding; i++) {
+
+        for (let j = 0; j < binding; j++) {
+
+            if (j%2 === 0) {
+                outputString = outputString + blackCell;
+            } else {
+                outputString = outputString + whiteCell;
+            }
+    
         }
 
-        if (i%binding === 0) {
-            let temp = whiteCell;
-            whiteCell = blackCell;
-            blackCell = temp;
-            outputString = outputString + lineBreaker;
-        }
+        let temp = whiteCell;
+        whiteCell = blackCell;
+        blackCell = temp;
+        outputString = outputString + lineBreaker;
     }
 
     console.log(outputString);
 }
+
+/**
+ * Alternatiive solution
+ */
+// const chessboard2 = (binding = 8) => {
+//     const lineBreaker = '\n';
+//     const gridTotal = binding * binding;
+
+//     let whiteCell = ' ';
+//     let blackCell = '#';
+//     let outputString = '';
+
+//     for (let i = 1; i <= gridTotal; i++) {
+        
+//         if (i%2 === 0) {
+//             outputString = outputString + blackCell;
+//         } else {
+//             outputString = outputString + whiteCell;
+//         }
+
+//         if (i%binding === 0) {
+//             let temp = whiteCell;
+//             whiteCell = blackCell;
+//             blackCell = temp;
+//             outputString = outputString + lineBreaker;
+//         }
+//     }
+
+//     console.log(outputString);
+// }
 
 module.exports = chessboard;
