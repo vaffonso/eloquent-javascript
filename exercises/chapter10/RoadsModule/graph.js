f3e699a60c81369882bced0e3602b4ad472ca326
vaffonso <vaffonso@br.ibm.com>
2020-01-29 22:23:46 -0300

    chapter 10

---
+++ exercises/chapter10/RoadsModule/graph.js
@@ -0,0 +1,15 @@
+export function buildGraph(edges) {
+  const graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (const [from, to] of edges) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
